cde17427a0190ad84ae442c5d9e5e94164c1c52d
Tonya Napoli <tqwerty05@gmail.com>
2024-02-29 10:21:23 -0500

    spinner test 1

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,25 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import React from 'react'
+import { render, screen } from `@testing-library/react`
+import `@testing-library/jest-dom`
+import Spinner from './Spinner'
+
+
 test('sanity', () => {
   expect(true).toBe(false)
 })
+
+//does spinner render when 'on' prop is true?
+test('spinner renders when on prop is true', () => {
+  render((<Spinner on={true} />));
+  const spinnerElement = screen.getByTestId(/Please wait.../i);
+  expect(spinnerElement).toBeInTheDocument();
+})
+
+//test taht spinner does not render when 'on' prop is false 
+test('spinner does not render when on prop is false', () => {
+  render(<Spinner on={false} />);
+  const spinnerElement = screen.queryByTestId(/Please wait.../i);
+  expect(spinnerElement).not.toBeInTheDocument();
+});
